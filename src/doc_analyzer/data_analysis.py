7e505722b8d9e4d9ed5ad1355121ce48d4ecccc7
Vedant Gupta <vedugpt77@gmail.com>
2025-08-04 12:13:42 -0400

    document analyzer doe updated

--- src/doc_analyzer/data_analysis.py
+++ src/doc_analyzer/data_analysis.py
@@ -1,10 +1,11 @@
-import os
+import os, sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+from prompt_library.prompts import PROMPT_REGISTRY
 
 class DocumentAnalyzer:
     """
@@ -13,7 +14,42 @@ class DocumentAnalyzer:
     """
 
     def __init__(self):
-        pass
+        self.log = CustomLogger().get_logger(__name__)
+        try:
+            self.loader = ModelLoader()
+            self.llm = self.loader.load_llm()
 
-    def analyze_document(self, document_path: str):
-        pass    
+            # Prepare parsers
+            self.parser = JsonOutputParser(pydantic_object=Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+
+            self.prompt = PROMPT_REGISTRY["document_analysis_prompt"]
+
+            self.log.info('DocumentAnalyzer initialized successfully')
+
+        except Exception as e:
+            self.log.error(f"Error initializing DocumentAnalyzer: {str(e)}")
+            raise DocumentPortalException("Failed to initialize DocumentAnalyzer", e) from e #type: ignore
+
+
+    def analyze_document(self, document_text: str):
+        """
+        Analyze a document's text and extract metadata & summary.
+        """
+        try:
+            chain = self.prompt | self.llm | self.fixing_parser
+
+            self.log.info("Meta-data analysis chain initialized")
+
+            response = chain.invoke({
+                "format_instructions": self.parser.get_format_instructions(),
+                "document_text": document_text
+            })
+
+            self.log.info("Metadata extraction successful", keys=list(response.keys()))
+
+            return response
+        
+        except Exception as e:
+            self.log.error(f"Metadata analysis failed", error=str(e))
+            raise DocumentPortalException("Metadata extraction failed", sys) #type: ignore
--- src/doc_analyzer/data_ingestion.py
+++ src/doc_analyzer/data_ingestion.py
@@ -12,12 +12,40 @@ class DocumentHandler:
     Automatically logs all actions and supports session-based organization.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, data_dir = None, session_id = None):
+        self.log = CustomLogger().get_logger(__name__)
+        self.data_dir = data_dir or os.getenv(
+            "DATA_STRORAGE_PATH",
+            os.path.join(os.getcwd(), "data", "document_analysis")
+        )
+        self.session_id = session_id or f"session_{datetime.utcnow().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
 
-    def save_pdf(self):
-        pass
+        # Create base session directory
+        self.session_path = os.path.join(self.data_dir, self.session_id)
+        os.makedirs(self.session_path, exist_ok=True)
 
-    def read_pdf(self):
-        pass
-    
+        self.log.info("PDFHandler initialized", session_id=self.session_id, session_path=self.session_path)
+
+    def save_pdf(self, pdf_data):
+        try:
+            pdf_path = os.path.join(self.data_dir, f"{self.session_id}.pdf")
+            with open(pdf_path, "wb") as pdf_file:
+                pdf_file.write(pdf_data)
+            self.log.info("PDF saved successfully", pdf_path=pdf_path)
+        except Exception as e:
+            self.log.error(f"Error saving PDF: {str(e)}")
+            raise DocumentPortalException("Failed to save PDF}", e) from e #type: ignore
+
+    def read_pdf(self, pdf_path):
+        try:
+            text_chunks = []
+            with fitz.open(pdf_path) as doc:
+                for page_num, page in enumerate(doc, start=1): #type: ignore
+                    text_chunks.append(f"\n--- Page {page_num} ---\n{page.get_text()}")
+                text = "\n".join(text_chunks)
+
+                self.log.info("PDF read successfully", pdf_path=pdf_path, session_id=self.session_id)
+            return text
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {str(e)}")
+            raise DocumentPortalException("Failed to read PDF", e) from e #type: ignore
